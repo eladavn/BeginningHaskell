125937797bda4d0d8fdfa8d1b60fb96df3684e07
eladavn <eladavn@gmail.com>
2015-04-09 13:34:13 +0300

    .

--- src/Chapter2/SimpleFunctions.hs
+++ src/Chapter2/SimpleFunctions.hs
@@ -6,7 +6,6 @@ firstOrEmpty lst = if not (null lst) then head lst else "empty"
 (+++) :: [a] -> [a] -> [a]
 lst1 +++ lst2  = if  null lst1 then lst2 else head lst1 : (tail lst1 +++ lst2)  
 
---A  commont
 reverse2 :: [t] -> [t]
 reverse2 lst =  if null lst then [] else reverse2 (tail lst) +++ [head lst] 
 
