bab0da7bac22f6fb7369ae133785039708604d14
eladavn <eladavn@gmail.com>
2015-04-12 00:53:37 +0300

    .

--- src/Chapter3/ParametricPolymorphism.hs
+++ src/Chapter3/ParametricPolymorphism.hs
@@ -3,3 +3,16 @@ module Chapter3.ParametricPolymorphism where
 swapTriple ::(t,t1,t2) -> (t1,t2,t)
 swapTriple (x,y,z) = (y,z,x)
 
+nothing :: a -> Maybe a
+nothing _ = Nothing
+
+maybeA ::[t] -> Char
+maybeA [] = 'a'
+
+index ::  [t] -> [(Int, t)]
+index [] = []
+index [x] = [(0,x)]
+index (x:xs) = let (n,_):_ = index xs
+               in (n+1,x):(index xs)
+
+
