0da0aef7ce5397ed213155984ef50864cb525794
eladavn <eladavn@gmail.com>
2015-04-17 15:53:58 +0300

    .

--- src/Chapter3/DivingIntoLists.hs
+++ src/Chapter3/DivingIntoLists.hs
@@ -10,7 +10,26 @@ myFoldl :: (item -> acc ->  acc) ->  acc -> [item] -> acc
 myFoldl _ acc [] = acc
 myFoldl f acc (item:items) = myFoldl f (f item acc) items 
   
+myProduct :: [Int] -> Int
+myProduct xs  = foldl (*) 1 xs
+
+findShortest :: [String] -> Maybe String
+findShortest []  = Nothing
+findShortest [x] = Just x
+findShortest (x:y:xs)
+    |  length x < length y = findShortest (x:xs)
+    |  otherwise = findShortest (y:xs)
     
+shortest :: String -> String -> String
+shortest x y
+    | length x < length y = x
+    | otherwise = y
+
+findShortest' :: [String] -> Maybe String
+findShortest' []  = Nothing
+findShortest' xs = Just $ foldl1 (shortest) xs 
+   
+ 
     
     
 
